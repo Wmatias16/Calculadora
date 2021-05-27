2a9eb4260ec3493fd5932ce95a46a07b6e5ec98e
Wmatias16 <51428438+Wmatias16@users.noreply.github.com>
2021-05-27 11:56:38 -0300

    Operaciones

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log("Hola mundo");
+let resultado = document.getElementById('resultado');
+let opciones = document.querySelectorAll('td');
+
+opciones.forEach(e =>{
+
+    e.addEventListener('click',()=>{
+        if(e.innerHTML == '='){
+            resultado.innerHTML = eval(resultado.innerHTML)
+        }else{
+            resultado.innerHTML += e.innerHTML
+        }
+        
+    })
+})
